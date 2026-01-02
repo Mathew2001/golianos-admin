374fdd925b1a4ddcc62858d8359b7ce9ae9924e4
Mathew2001 <mathew2001abed@gmail.com>
2026-01-02 16:51:25 +0200

    ad

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
           {/* protected area */}
           <Route element={<PrivateRoute />}>
-            <Route index element={<Layout />}>
+            <Route element={<Layout />}>
               {routes.map((route, index) => (
                 <Route key={index} path={route.path} element={route.element} />
               ))}
